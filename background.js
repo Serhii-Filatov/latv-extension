458d9efb9a9d2d42b7d9ba649d4e4010943bd72c
Serhii-Filatov <55346150+Serhii-Filatov@users.noreply.github.com>
2024-06-19 12:12:00 +0300

    added following links using js

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed.");
 });
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "openUrlInCurrentTab") {
+    chrome.tabs.update(sender.tab.id, { url: request.url });
+    sendResponse({ status: "URL opened in current tab" });
+  }
+});
