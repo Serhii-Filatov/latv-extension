7124d71828550a3609fc18d9dc63064c0b27fa42
Serhii-Filatov <55346150+Serhii-Filatov@users.noreply.github.com>
2024-06-24 18:42:35 +0300

    added a proxy and 2 test buttons

--- background.js
+++ background.js
@@ -1,5 +1,40 @@
+const proxyConfig = {
+  mode: "pac_script",
+  pacScript: {
+    data: `
+      function FindProxyForURL(url, host) {
+        const proxy = 'PROXY 104.248.16.121:3128';
+        const direct = 'DIRECT';
+        const proxyHosts = [
+          'bbc.co.uk/iplayer',
+          'itv.com',
+          'channel4.com',
+          'tvplayer.com',
+          '2ip.ua'
+        ];
+
+        for (let i = 0; i < proxyHosts.length; i++) {
+          if (dnsDomainIs(host, proxyHosts[i])) {
+            return proxy;
+          }
+        }
+        return direct;
+      }
+    `
+  }
+};
+
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed.");
+  chrome.proxy.settings.set(
+    {value: proxyConfig, scope: 'regular'},
+    function() {
+      if (chrome.runtime.lastError) {
+        console.error(`Error setting proxy: ${chrome.runtime.lastError}`);
+      } else {
+        console.log("Proxy settings applied successfully");
+      }
+    }
+  );
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
--- popup.js
+++ popup.js
@@ -1,15 +1,16 @@
-
-
 const urlMap = {
   iplayer: "https://www.bbc.co.uk/iplayer",
   itv: "https://www.itv.com/",
   channel4: "https://www.channel4.com/",
-  tvplayer: "https://tvplayer.com/"
+  tvplayer: "https://tvplayer.com/",
+  ip: "https://2ip.ua/ru",
+  ip2: "https://whoer.net/ru"
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', () => {
+      
       const url = urlMap[button.id];
       if (url) {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
